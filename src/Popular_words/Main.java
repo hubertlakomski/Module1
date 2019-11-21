c2beb3be67c59b3acb6bc42449628a88940183f1
Hubert Łakomski <hubertlakomski@gmail.com>
2019-11-21 12:44:07 +0100

    done

--- src/Popular_words/Main.java
+++ src/Popular_words/Main.java
@@ -18,12 +18,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        //Arrays of websites links and their header designation
         Connection[] websites = new Connection[3];
         String[] cssQuery = new String[3];
 
+        //path to creating files
         Path path1 = Paths.get("popular_words.txt");
         Path path2 = Paths.get("filtered_popular_words.txt");
 
+        //links to websites and html header designation
         websites[0] = Jsoup.connect("https://www.onet.pl/");
         cssQuery[0] = "span.title";
         websites[1] = Jsoup.connect("https://www.spidersweb.pl/");
@@ -31,26 +34,33 @@ public class Main {
         websites[2] = Jsoup.connect("https://gazetawroclawska.pl/");
         cssQuery[2] = "h2";
 
+        //word list excluded from the output file
         String[] excluded = {"Obrady", "statutowa"};
 
-
+        //creating file with words longer then 3 characters
         wordsToFile(websites, cssQuery, path1);
 
-        Files.deleteIfExists(path2);
-        Files.createFile(path2);
-
-        List<String> words = Files.readAllLines(path1);
-
-        for(String word: words){
-            if(!checkExclusion(word, excluded)){
-                Files.write(path2, Collections.singletonList(word), StandardOpenOption.APPEND);
-            }
-        }
-
+        //creating a new file from the file created in the previous method without excluded words
+        writeWithoutExclusion(path1, path2, excluded);
 
     }
 
-    public static boolean checkExclusion(String word, String[] excluded){
+    public static void writeWithoutExclusion(Path fileFromWrite, Path fileToWrite, String[] excluded) throws IOException {
+
+        Files.deleteIfExists(fileToWrite);
+        Files.createFile(fileToWrite);
+
+        List<String> words = Files.readAllLines(fileFromWrite);
+
+        for(String word: words){
+            if(!checkExclusion(word, excluded)){
+                Files.write(fileToWrite, Collections.singletonList(word), StandardOpenOption.APPEND);
+            }
+        }
+
+    }
+
+    private static boolean checkExclusion(String word, String[] excluded){
 
         for(String excludedWord: excluded){
             if(word.equals(excludedWord)){
@@ -67,16 +77,21 @@ public class Main {
         Files.createFile(path);
 
         for (int i=0; i<websites.length; i++) {
+
+            //loading headers from the website
             Document document = websites[i].get();
 
             Elements links = document.select(cssQuery[i]);
 
             for (Element elem : links) {
 
+                //dividing header into words
                 String tmp = elem.text();
                 String[] words = tmp.split(" ");
 
                 for(String word: words) {
+
+                    //writing only words longer then 3 characters to the file
                     if(word.length()>3) {
                         Files.write(path, Collections.singletonList(word), StandardOpenOption.APPEND);
                     }
